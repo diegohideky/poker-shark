a96d2502e1c98d4c2b8a67b635d6fed072e6271a
Diego Hideky de Oliveira Lima <diegohideky@gmail.com>
2023-12-31 19:26:39 -0300

    Fix

--- libs/googleapi.js
+++ libs/googleapi.js
@@ -3,40 +3,7 @@ import { google } from "googleapis";
 let client = null;
 let auth = null;
 
-export function getAuth() {
-  if (auth) {
-    return auth;
-  }
-
-  auth = new google.auth.GoogleAuth({
-    credentials: {
-        client_email: process.env.GOOGLE_CLIENT_EMAIL,
-        client_id: process.env.GOOGLE_CLIENT_ID,
-        private_key: process.env.GOOGLE_PRIVATE_KEY,
-    },
-    scopes: ["https://www.googleapis.com/auth/spreadsheets"],
-  });
-
-  return auth;
-}
-
-export async function getClient() {
-  if (client) {
-      return client;
-  }
-
-  auth = getAuth();
-
-  client = await auth.getClient();
-
-  return client;
-}
-
 export async function getSheets() {
-  if (client) {
-    return client;
-  }
-
   client = await getClient();
 
   const googleSheets = google.sheets({ version: "v4", auth: client });
@@ -45,13 +12,36 @@ export async function getSheets() {
 }
 
 export async function getSheetData() {
-  const sheets = await getSheets();
+  const credentials = {
+    client_email: process.env.GOOGLE_CLIENT_EMAIL,
+    client_id: process.env.GOOGLE_CLIENT_ID,
+    private_key: process.env.GOOGLE_PRIVATE_KEY,
+  }
 
-  const result = await sheets?.spreadsheets?.values?.get({
-    auth: getAuth(),
+  console.log({ credentials });
+  
+  const auth = new google.auth.GoogleAuth({
+    credentials,
+    scopes: ["https://www.googleapis.com/auth/spreadsheets"],
+  });
+
+  console.log({ auth });
+
+  const client = await auth.getClient();
+
+  console.log({ client });
+
+  const sheets = await google.sheets({ version: "v4", auth: client });
+
+  console.log({ sheets });
+
+  const result = await sheets.spreadsheets?.values?.get({
+    auth,
     spreadsheetId: process.env.SPREADSHEET_ID,
     range: "Estatísticas!C3:E25"
   });
+
+  console.log({ result });
 
   return result?.data?.values || [];
 }
