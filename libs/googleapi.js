dff1bb704577474e3ce5f0c4f3060ee5b2cd43c4
Diego Hideky de Oliveira Lima <diegohideky@gmail.com>
2024-01-02 09:51:47 -0300

    Another fix

--- libs/googleapi.js
+++ libs/googleapi.js
@@ -15,7 +15,7 @@ export async function getSheetData() {
   const credentials = {
     client_email: process.env.GOOGLE_CLIENT_EMAIL,
     client_id: process.env.GOOGLE_CLIENT_ID,
-    private_key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+    private_key: process.env.GOOGLE_PRIVATE_KEY?.split(String.raw`\n`).join('\n'),
   }
 
   console.log({ credentials });
