159d1bcbea6f200e05da7b2c0ada9d2138c5d3b2
Diego Hideky de Oliveira Lima <diegohideky@gmail.com>
2024-04-06 19:05:03 -0300

    Put it back the players

--- libs/items.js
+++ libs/items.js
@@ -1,3 +1,81 @@
+export const PLAYERS = {
+  Ademir: {
+    image: "/players/Ademir.png",
+  },
+  Allan: {
+    image: "/players/Allan.png",
+  },
+  Arnaldo: {
+    image: "/players/Arnaldo.png",
+  },
+  Bruno: {
+    image: "/players/Bruno.png",
+  },
+  Demori: {
+    image: "/players/Demori.png",
+  },
+  Diego: {
+    image: "/players/Diego.png",
+  },
+  Eder: {
+    image: "/players/Eder.png",
+  },
+  Erik: {
+    image: "/players/Erik.png",
+  },
+  Felipe: {
+    image: "/players/Felipe.png",
+  },
+  Gilson: {
+    image: "/players/Gilson.png",
+  },
+  Giu: {
+    image: "/players/Giu.png",
+  },
+  Glauco: {
+    image: "/players/Glauco.png",
+  },
+  Gustavo: {
+    image: "/players/Gustavo.png",
+  },
+  Leonardo: {
+    image: "/players/Leonardo.png",
+  },
+  Luiz: {
+    image: "/players/Luiz.png",
+  },
+  Marcio: {
+    image: "/players/Marcio.png",
+  },
+  Marco: {
+    image: "/players/Marco.png",
+  },
+  Marquinho: {
+    image: "/players/Marquinho.png",
+  },
+  Rafael: {
+    image: "/players/Rafael.png",
+  },
+  Reinaldo: {
+    image: "/players/Reinaldo.png",
+  },
+  Rodolfo: {
+    image: "/players/Rodolfo.png",
+  },
+  Vianna: {
+    image: "/players/Vianna.png",
+  },
+  Vinicius: {
+    image: "/players/Vinicius.png",
+  },
+  Waldeir: {
+    image: "/players/Waldeir.png",
+  },
+  Wilker: {
+    image: "/players/Wilker.png",
+  },
+};
+
 export const BLINDS = [
   {
     small: 25,
