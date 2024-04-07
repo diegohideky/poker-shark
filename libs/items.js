ecc2fb8e1ec930e5e1a4f44386960628d4c19306
Diego Hideky de Oliveira Lima <diegohideky@gmail.com>
2024-04-07 08:12:57 -0300

    Fixed buttons

--- libs/items.js
+++ libs/items.js
@@ -78,93 +78,93 @@ export const PLAYERS = {
 
 export const BLINDS = [
   {
-    small: 25,
-    big: 50,
-    duration: 10,
-  },
-  {
     small: 50,
     big: 100,
-    duration: 10,
+    duration: 15,
   },
   {
     small: 100,
     big: 200,
-    duration: 10,
+    duration: 15,
   },
   {
     small: 150,
     big: 300,
-    duration: 10,
+    duration: 15,
   },
   {
     small: 200,
     big: 400,
-    duration: 10,
+    duration: 15,
   },
   {
     small: 250,
     big: 500,
-    duration: 10,
+    duration: 15,
   },
   {
     small: 300,
     big: 600,
-    duration: 10,
+    duration: 15,
   },
   {
     small: 400,
     big: 800,
-    duration: 10,
+    duration: 15,
   },
   {
     small: 500,
     big: 1000,
-    duration: 10,
+    duration: 15,
   },
   {
     small: 600,
     big: 1200,
-    duration: 10,
+    duration: 15,
   },
   {
     small: 800,
     big: 1600,
-    duration: 10,
+    duration: 15,
   },
   {
     small: 1000,
     big: 2000,
-    duration: 10,
+    duration: 15,
   },
   {
     small: 1200,
     big: 2400,
-    duration: 10,
+    duration: 15,
   },
   {
     small: 1500,
     big: 3000,
-    duration: 10,
+    duration: 15,
   },
   {
     small: 2000,
     big: 4000,
-    duration: 10,
+    duration: 15,
   },
   {
     small: 2500,
     big: 5000,
-    duration: 10,
+    duration: 15,
   },
   {
     small: 3000,
     big: 6000,
-    duration: 10,
+    duration: 15,
   },
   {
     small: 4000,
     big: 8000,
-    duration: 10,
+    duration: 15,
+  },
+  {
+    small: 5000,
+    big: 10000,
+    duration: 15,
   },
 ];
--- pages/clock/index.js
+++ pages/clock/index.js
@@ -1,9 +1,10 @@
 import { useState, useEffect } from "react";
 import Head from "next/head";
 import { FiPause, FiPlay } from "react-icons/fi";
-import { FaArrowRotateRight } from "react-icons/fa6";
-import { GrConfigure } from "react-icons/gr";
+import { FaArrowRotateRight, FaTrash } from "react-icons/fa6";
+import { GrCaretNext, GrCaretPrevious, GrConfigure } from "react-icons/gr";
 import { FaRegClock } from "react-icons/fa6";
+import { IoIosAdd } from "react-icons/io";
 import { BLINDS } from "../../libs/items";
 import styles from "../../styles/Home.module.css";
 
@@ -17,16 +18,16 @@ const TimerBox = ({ value, label, bgColor = "bg-orange-700" }) => {
   return (
     <div className="flex flex-col gap-2">
       <div
-        className={`flex justify-center items-center border border-white rounded-md sm:p-6 p-4 ${bgColor} transition ease-in-out bg-opacity-50 w-[70px] sm:w-[150px] sm:w-[200px] md:w-[200px] h-[70px] sm:h-[150px] md:h-[200px]`}
+        className={`flex justify-center items-center border border-white rounded-md sm:p-6 p-4 ${bgColor} transition ease-in-out bg-opacity-50 w-[150px] sm:w-[150px] sm:w-[200px] md:w-[200px] h-[150px] sm:h-[150px] md:h-[200px]`}
       >
         <div
           suppressHydrationWarning
-          className="w-full flex items-center justify-center text-3xl sm:text-5xl md:text-8xl font-semibold text-white"
+          className="w-full flex items-center justify-center text-7xl sm:text-7xl md:text-8xl font-semibold text-white"
         >
           {value}
         </div>
       </div>
-      <div className="w-full flex items-center justify-center text-[10px] sm:text-[18px] text-white">
+      <div className="w-full flex items-center justify-center text-[15px] sm:text-[18px] text-white">
         {label}
       </div>
     </div>
@@ -42,10 +43,14 @@ export default function Home() {
   const [currentBlindIndex, setCurrentBlindIndex] = useState(0);
   const [hasPaused, setHasPaused] = useState(false);
 
-  const playClock = (remainingMinutes, remainingSeconds) => {
-    setHasStarted(true);
-    setHasPaused(false);
+  const getCurrentMinAndSeconds = (blind) => {
+    const totalTimeSeconds = blind.duration * 60;
+    const minutes = Math.floor(totalTimeSeconds / 60);
+    const seconds = totalTimeSeconds % 60;
+    return { totalTimeSeconds, minutes, seconds };
+  };
 
+  const playClock = (remainingMinutes, remainingSeconds) => {
     let totalTimeSeconds;
 
     if (remainingMinutes || remainingSeconds) {
@@ -53,13 +58,15 @@ export default function Home() {
       setTimerMinutes(remainingMinutes);
       setTimerSeconds(remainingSeconds);
     } else {
-      totalTimeSeconds = blinds[currentBlindIndex].duration * 60;
-      const minutes = Math.floor(totalTimeSeconds / 60);
-      const seconds = totalTimeSeconds % 60;
+      const result = getCurrentMinAndSeconds(blinds[currentBlindIndex]);
+      totalTimeSeconds = result.totalTimeSeconds;
 
-      setTimerMinutes(minutes);
-      setTimerSeconds(seconds);
+      setTimerMinutes(result.minutes);
+      setTimerSeconds(result.seconds);
     }
+
+    setHasStarted(true);
+    setHasPaused(false);
 
     const id = setInterval(() => {
       if (totalTimeSeconds > 0) {
@@ -92,31 +99,70 @@ export default function Home() {
     playClock();
   };
 
-  const handleConfigure = () => {
+  const handleConfigure = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    clearInterval(intervalId);
     setTimerMinutes(0);
     setTimerSeconds(0);
     setCurrentBlindIndex(0);
-    clearInterval(intervalId);
     setHasStarted(false);
   };
 
-  const handleRestart = () => {
-    setTimerMinutes(0);
-    setTimerSeconds(0);
-    setCurrentBlindIndex(0);
+  const handlePrevious = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
     clearInterval(intervalId);
+
+    setHasPaused(true);
+    if (currentBlindIndex > 0) {
+      setCurrentBlindIndex((prev) => prev - 1);
+      const { minutes, seconds } = getCurrentMinAndSeconds(
+        blinds[currentBlindIndex - 1]
+      );
+      setTimerMinutes(minutes);
+      setTimerSeconds(seconds);
+    }
+  };
+
+  const handleNext = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    clearInterval(intervalId);
+    setHasPaused(true);
+    if (currentBlindIndex < blinds.length - 1) {
+      setCurrentBlindIndex((prev) => prev + 1);
+      const { minutes, seconds } = getCurrentMinAndSeconds(
+        blinds[currentBlindIndex + 1]
+      );
+      setTimerMinutes(minutes);
+      setTimerSeconds(seconds);
+    }
+  };
+
+  const handleRestart = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    clearInterval(intervalId);
+    const { minutes, seconds } = getCurrentMinAndSeconds(
+      blinds[currentBlindIndex]
+    );
+    setTimerMinutes(minutes);
+    setTimerSeconds(seconds);
     setHasStarted(false);
-    clearInterval(intervalId);
-    setCurrentBlindIndex(0);
     playClock();
   };
 
-  const handlePause = () => {
+  const handlePause = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
     clearInterval(intervalId);
     setHasPaused(true);
   };
 
-  const handleUnpause = () => {
+  const handleUnpause = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
     playClock(timerMinutes, timerSeconds);
   };
 
@@ -142,9 +188,7 @@ export default function Home() {
   };
 
   const getTextColor = (timerMinutes, timerSeconds) => {
-    // check if that's 10 percent of the increaseDuration
     const totalSeconds = blinds[currentBlindIndex].duration * 60;
-    // if thats 10 percent of less then return bg-red
     if (timerMinutes === 0 && timerSeconds <= totalSeconds * 0.1) {
       return "text-red-500";
     } else if (timerMinutes === 0 && timerSeconds <= totalSeconds * 0.2) {
@@ -164,6 +208,29 @@ export default function Home() {
     }
 
     setBlinds([...blinds]);
+  };
+
+  const handleRemoveBlind = (index) => (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    const newBlinds = blinds.filter((blind, i) => i !== index);
+    setBlinds([...newBlinds]);
+  };
+
+  const handleAddNewBlind = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    setBlinds([
+      ...blinds,
+      {
+        small: 0,
+        big: 0,
+        duration: 0,
+        smallError: "Preencha os campos",
+        bigError: "Preencha os campos",
+        durationError: "Preencha os campos",
+      },
+    ]);
   };
 
   return (
@@ -214,14 +281,18 @@ export default function Home() {
         <section>
           {!hasStarted && (
             <div className="flex flex-col items-center justify-center p-5 md:p-10 w-full gap-2">
-              <div className="flex flex-row justify-center items-center gap-2 text-white">
-                <span className="w-full md:w-[50px]">Level</span>
+              <div className="flex flex-row justify-center items-center gap-2 text-white w-full">
+                <span className="w-full md:w-[50px]">Nível</span>
                 <span className="w-full md:w-[150px]">Small</span>
                 <span className="w-full md:w-[150px]">Big</span>
-                <span className="w-full md:w-[150px]">Duration (min)</span>
+                <span className="w-full md:w-[150px]">Duração (min)</span>
+                <span className="w-full md:w-[150px]">.</span>
               </div>
               {blinds.map((blind, index) => (
-                <div className="flex flex-row gap-2 items-center" key={index}>
+                <div
+                  className="flex flex-row gap-2 justify-center items-center"
+                  key={index}
+                >
                   <span className="w-full md:w-[50px] text-white">
                     {index + 1} -{" "}
                   </span>
@@ -254,19 +325,33 @@ export default function Home() {
                       onChange(e.target.value, "duration", index)
                     }
                   />
+                  <button
+                    className="flex flex-row items-center transition ease-in-out gap-2 p-4 py-2 bg-red-500 text-white rounded-full hover:bg-red-700 hover:outline-none"
+                    onClick={handleRemoveBlind(index)}
+                  >
+                    <FaTrash />
+                  </button>
                 </div>
               ))}
-              <button
-                className="flex flex-row items-center gap-2 min-w-[110px] mt-3 px-4 py-2 bg-blue-500 text-white rounded-lg"
-                onClick={handleStart}
-              >
-                <FaRegClock />
-                Iniciar
-              </button>
+              <div className="mt-3 flex flex-row gap-2 items-center justify-center p-2">
+                <button
+                  className="flex flex-row items-center gap-2 min-w-[110px] px-4 py-2 bg-blue-500 text-white rounded-lg"
+                  onClick={handleStart}
+                >
+                  <FaRegClock />
+                  Iniciar
+                </button>
+                <button
+                  className={`flex flex-row items-center transition ease-in-out p-3 bg-white text-black rounded-full hover:bg-gray-500 hover:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-opacity-50`}
+                  onClick={handleAddNewBlind}
+                >
+                  <IoIosAdd />
+                </button>
+              </div>
             </div>
           )}
           {hasStarted && (
-            <div className="flex flex-col items-center justify-center p-5 md:p-10 w-full">
+            <div className="flex flex-col items-center justify-center p-5 md:p-20 w-full">
               <div className="flex justify-items-center items-center mt-6 gap-2">
                 <TimerBox
                   key={"MINUTOS"}
@@ -281,10 +366,10 @@ export default function Home() {
                   bgColor={getBgColor(timerMinutes, timerSeconds)}
                 />
               </div>
-              <div className="flex justify-items-center items-center mt-6 gap-10 py-10">
+              <div className="flex justify-items-center items-center mt-6 gap-10 py-2 md:py-10">
                 <div className="flex flex-col justify-center items-center">
                   <span
-                    className={`text-2xl md:text-[70px] font-bold transition ease-in-out ${getTextColor(
+                    className={`text-4xl md:text-[70px] font-bold transition ease-in-out ${getTextColor(
                       timerMinutes,
                       timerSeconds
                     )}`}
@@ -296,42 +381,69 @@ export default function Home() {
               </div>
               <div className="flex flex-row gap-2">
                 <button
-                  className="flex flex-row items-center transition ease-in-out gap-2 min-w-[110px] mt-7 px-4 py-2 bg-gray-500 text-white rounded-lg hover:bg-gray-700 hover:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-opacity-50"
+                  className="flex flex-row items-center transition ease-in-out gap-2 mt-7 p-4 bg-white text-black rounded-full hover:bg-gray-500 hover:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-opacity-50"
                   onClick={handleConfigure}
                 >
                   <GrConfigure />
-                  Configurar
+                </button>
+                <button
+                  className={`flex flex-row items-center transition ease-in-out mt-7 p-4 bg-white text-black rounded-full hover:bg-gray-500 hover:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-opacity-50 ${
+                    currentBlindIndex === 0 && "opacity-50"
+                  }`}
+                  onClick={handlePrevious}
+                  disabled={currentBlindIndex === 0}
+                >
+                  <GrCaretPrevious />
                 </button>
 
                 {!hasPaused ? (
                   <button
-                    className="flex flex-row items-center transition ease-in-out gap-2 min-w-[110px] mt-7 px-4 py-2 bg-red-500 text-white rounded-lg hover:bg-red-700 hover:outline-none"
+                    className="flex flex-row items-center transition ease-in-out gap-2 mt-7 p-4 py-2 bg-red-500 text-white rounded-full hover:bg-red-700 hover:outline-none"
                     onClick={handlePause}
                   >
                     <FiPause />
-                    Pausar
                   </button>
                 ) : (
                   <button
-                    className="flex flex-row items-center transition ease-in-out gap-2 min-w-[110px] mt-7 px-4 py-2 bg-green-500 text-white rounded-lg hover:bg-green-700 hover:outline-none"
+                    className="flex flex-row items-center transition ease-in-out gap-2 mt-7 p-4 py-2 bg-green-500 text-white rounded-full hover:bg-green-700 hover:outline-none"
                     onClick={handleUnpause}
                   >
                     <FiPlay />
-                    Continuar
                   </button>
                 )}
+
                 <button
-                  className="flex flex-row items-center transition ease-in-out gap-2 min-w-[110px] mt-7 px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-700 hover:outline-none"
+                  className={`flex flex-row items-center transition ease-in-out gap-2 mt-7 p-4 bg-white text-black rounded-full hover:bg-gray-500 hover:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-opacity-50 ${
+                    currentBlindIndex === blinds.length - 1 && "opacity-50"
+                  }`}
+                  onClick={handleNext}
+                  disabled={currentBlindIndex === blinds.length - 1}
+                >
+                  <GrCaretNext />
+                </button>
+
+                <button
+                  className="flex flex-row items-center transition ease-in-out gap-2 mt-7 p-4 bg-white text-black rounded-full hover:bg-gray-500 hover:outline-none"
                   onClick={handleRestart}
                 >
                   <FaArrowRotateRight />
-                  Recomeçar
                 </button>
+              </div>
+              <div className="flex flex-col gap-2 mt-8 border border-white p-4 rounded-lg transition ease-in-out duration-300">
+                <span className="text-white">
+                  Nível: {currentBlindIndex + 1}
+                </span>
+                <span className="text-white">
+                  Duração: {blinds[currentBlindIndex].duration} minutos
+                </span>
+                <span className="text-white">
+                  Próximo: {blinds[currentBlindIndex + 1]?.small || "-"} /{" "}
+                  {blinds[currentBlindIndex + 1]?.big || "-"}
+                </span>
               </div>
             </div>
           )}
         </section>
-        <section className="flex flex-row items-center justify-center p-10 md:p-20"></section>
       </main>
     </div>
   );
